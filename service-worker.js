87288e22f2b0b914c45626a40002d55fd4b979d5
Atia-Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-06-15 23:15:14 +0600

    first commit

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "todo-app-cache-v1";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./src/script.js",
+    "./site.webmanifest",
+    "./assets/favicons/web-app-manifest-192x192.png",
+    "./assets/favicons/web-app-manifest-512x512.png",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css",
+    "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js"
+];
+
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    self.clients.claim();
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return (
+                cachedResponse ||
+                fetch(event.request).catch(() => {
+                    if (event.request.mode === "navigate") {
+                        return caches.match("./index.html");
+                    }
+                })
+            );
+        })
+    );
+});
---
+++ src/script.js
@@ -0,0 +1,82 @@
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const deleteCheckedBtn = document.getElementById('deleteCheckedBtn');
+const taskList = document.getElementById('taskList');
+const emptyTaskList = document.getElementById('empty-tasklist');
+
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+renderTasks();
+
+addBtn.addEventListener('click', () => {
+    const task = taskInput.value.trim();
+
+    if (task !== "") {
+        const newTask = {
+            text: task,
+            completed: false
+        };
+        tasks.push(newTask);
+
+        saveTasks();
+        renderTasks();
+
+        taskInput.value = '';
+    }
+    else {
+        alert("Please add a task.");
+    }
+});
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+
+    saveTasks();
+    renderTasks();
+};
+
+deleteCheckedBtn.addEventListener('click', () => {
+    tasks = tasks.filter((task) => !task.completed);
+
+    saveTasks();
+    renderTasks();
+});
+
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function renderTasks() {
+    taskList.innerHTML = '';
+
+    if (tasks.length > 0) {
+        tasks.forEach((task, index) => {
+            emptyTaskList.classList.add('d-none');
+
+            const li = document.createElement('li');
+            li.className = `taskItem list-group-item d-flex justify-content-between align-items-center border-primary-subtle ${task.completed ? 'bg-primary-subtle' : ''}`;
+
+            li.innerHTML = `
+            <input type="checkbox" data-index="${index}" id="task-${index}" class="form-check-input mx-3 border-primary" ${task.completed ? 'checked' : ''}>
+            <label for="task-${index}" class="form-check-label flex-grow-1 ${task.completed ? 'text-decoration-line-through text-muted' : ''}">${task.text}</label>
+            <button onclick="deleteTask(${index})" class="btn text-danger">✘</button>
+        `;
+
+            taskList.appendChild(li);
+        });
+    }
+    else {
+        emptyTaskList.classList.remove('d-none');
+    }
+}
+
+taskList.addEventListener('change', (event) => {
+    if (event.target && event.target.matches('input[type="checkbox"]')) {
+        const index = event.target.getAttribute('data-index');
+        tasks[index].completed = event.target.checked;
+        saveTasks();
+        renderTasks();
+    }
+});
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
