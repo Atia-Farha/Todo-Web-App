aa138497a3e82404f96dfca3f0c0c7a5cf124f76
Atia-Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-06-21 17:30:05 +0600

    Fixed some issues

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = "todo-app-cache-v1";
 const ASSETS = [
-    "./",
-    "./index.html",
-    "./src/script.js",
-    "./site.webmanifest",
-    "./assets/favicons/web-app-manifest-192x192.png",
-    "./assets/favicons/web-app-manifest-512x512.png",
+    "/",
+    "/index.html",
+    "/src/script.js",
+    "/site.webmanifest",
+    "/assets/favicons/web-app-manifest-192x192.png",
+    "/assets/favicons/web-app-manifest-512x512.png",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css",
     "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js"
